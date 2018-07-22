654f720128f6b727933c6f95f02ff512228cd8ea
elseagle <dowolebolu@gmail.com>
2018-07-23 00:31:46 +0100

    bug fix

--- api_server.py
+++ api_server.py
@@ -10,19 +10,20 @@ CORS(app)
 api = Api(app, version='1.0', title='API for sentiment analysis', validate=False) 
 ns = api.namespace('api_server', 'Returns sentiments on string on twitter')
 
-model_input = api.model('Enter the keyword and no of tweets:', {"Keyword": fields.String()})
+model_input = api.model('Enter the company or celebrity here', {"Sentiment": fields.String()})
 port = int(os.getenv('PORT', 8080)) 
 
-@ns.route('/sieve') 
+@ns.route('/sieve', methods =["GET"]) 
 class SIEVE(Resource): 
     @api.response(200, "Success", model_input)   
     @api.expect(model_input)
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('Keyword', type=int)
+        parser.add_argument('Sentiment', type=int)
         args = parser.parse_args()
         inp = str(args["Keyword"]) 
         result = sc(inp)
-        return jsonify({"Sentiments": result}) 
-if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port=port, debug=False)
+        return jsonify(result) 
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))
