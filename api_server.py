665efd694715ed14a3863014b120de26ea233e40
elseagle <dowolebolu@gmail.com>
2018-07-23 01:30:35 +0100

    changed api route to 'sentiment'

--- api_server.py
+++ api_server.py
@@ -13,7 +13,7 @@ ns = api.namespace('api_server', 'Returns sentiments on string on twitter')
 model_input = api.model('Enter the company or celebrity here', {"Sentiment": fields.String()})
 port = int(os.getenv('PORT', 8080)) 
 
-@ns.route('/sieve') 
+@ns.route('/sentiment') 
 class SIEVE(Resource): 
     @api.response(200, "Success", model_input)   
     @api.expect(model_input)
